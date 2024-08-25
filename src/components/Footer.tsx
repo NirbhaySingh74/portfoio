6f58970b38f78363a0e3236df44943eae215a68b
Nirbhay <nirbhaysingh943012@gmail.com>
2024-08-25 13:06:11 +0530

    Footer updated

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,15 @@
 import React from "react";
-import { FaTwitter, FaGithub, FaLinkedinIn } from "react-icons/fa"; // Social media icons
+import { FaTwitter, FaGithub, FaLinkedinIn } from "react-icons/fa";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="w-full bg-black text-white py-6">
+    <footer
+      className="w-full py-6"
+      style={{
+        backgroundColor: "var(--card-background-color)",
+        color: "var(--text-color)",
+      }}
+    >
       <div className="container mx-auto px-4">
         <div className="flex flex-col md:flex-row justify-between items-center text-center md:text-left">
           <div className="mb-4 md:mb-0">
